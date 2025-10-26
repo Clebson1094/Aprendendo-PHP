2089c20679519f9a522b23411e85c7ecb3026a74
Clebson1094 <clebson167.cj@gmail.com>
2025-10-26 00:24:37 -0300

    Primeiros passos

---
+++ ex003/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php
+        // 0x = hexadecimal 0b = binário 0 = octal 
+        // $numero = 010;
+        // echo "O valor da variável é $numero";
+
+
+        $variavel = 300;
+        var_dump($variavel);
+    ?>
+</body>
+</html>
